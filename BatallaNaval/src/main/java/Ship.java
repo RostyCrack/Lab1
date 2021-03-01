76e5e7273a146421567bfb42693576f0b9b4eaee
SBarrera16 <79816580+SBarrera16@users.noreply.github.com>
2021-02-28 21:25:14 -0500

    Update Ship.java

--- BatallaNaval/src/main/java/Ship.java
+++ BatallaNaval/src/main/java/Ship.java
@@ -6,4 +6,21 @@ public class Ship {
     private int score;
     private ArrayList<Marine> marines;
     private Position location;
+    
+    public int numeroMaquinas() {
+
+        int numeroMaquinas;
+        return JOptionPane.showMessageDialog(null, "Nel numero de maquinas es", +numeroMaquinas())
+
+    }
+    
+    public boolean suficientesMarinos() {
+        for ships {
+            if Marine =>4 {
+
+                return JOptionPane.showMessageDialog(null, "Hay suficientes marinos para la conduccion de la nave");
+            }
+        else JOptionPane.showMessageDialog(null, "No hay suficientes marinos para la conduccion de la nave");
+        }
+     
 }
