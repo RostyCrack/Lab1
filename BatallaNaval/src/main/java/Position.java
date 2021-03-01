5b3e509d0af9b327f73235fc67635cc0a15d4fd0
SBarrera16 <79816580+SBarrera16@users.noreply.github.com>
2021-02-28 21:36:48 -0500

    Update Position.java

--- BatallaNaval/src/main/java/Position.java
+++ BatallaNaval/src/main/java/Position.java
@@ -2,4 +2,13 @@ public class Position {
 
     private int latitude;
     private int longitude;
+    public int deltaLontiud{
+        (latitude-MovimientoLo = deltaLongitud <100);
+    }
+        public int deltaLatitud{
+        (latitude-MovimientoLa = deltaLatitud <100);
+    }
+    
+    
+    public void muevase (int deltaLongitud, int deltaLatitud);
 }
